da3e4f72211a9fef6cb83dd41cec694817e0f4b2
Naveen Jayathilake <tharindunaveen9@gmail.com>
2025-02-08 04:09:06 +0530

    about us page

--- src/pages/AboutUs/AboutUs2.jsx
+++ src/pages/AboutUs/AboutUs2.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Box, Typography, IconButton, Grid } from "@mui/material";
+import { Box, Typography, IconButton, Grid, Avatar } from "@mui/material";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import UserProfileIcon from "../../assets/AboutUs/UserProfileIcon.png";
+import AboutUs2Image from "../../assets/AboutUs/AboutUs2.jpg";
 
 const AboutUs2 = () => {
   AOS.init({ duration: 1000 });
@@ -9,7 +11,6 @@ const AboutUs2 = () => {
   return (
     <Box
       sx={{
-        // minHeight: "100svh",
         mt: 5,
         mb: 5,
         display: "flex",
@@ -17,20 +18,32 @@ const AboutUs2 = () => {
         width: "100%",
       }}
     >
-      <Box sx={{ maxWidth: 1400, width: "100%", mx: "auto", mt: 3, px: 1 }}>
-        <Grid container>
+      <Box
+        sx={{
+          maxWidth: 1400,
+          width: "100%",
+          mx: "auto",
+          mt: 3,
+          px: 1,
+          display: "block",
+        }}
+      >
+        <Grid container spacing={0}>
           <Grid
             item
             xs={0}
             md={5}
             display={{ xs: "none", md: "block" }}
             height={"100%"}
+            width={"100%"}
+            pr={5}
           >
-            <Box
+            <Box component={"img"} src={AboutUs2Image} height={"100%"} mr={2} />
+            {/* <Box
               sx={{ bgcolor: "lightgrey", height: "100%", width: "100%" }}
-            ></Box>
+            ></Box> */}
           </Grid>
-          <Grid item md={7}>
+          <Grid item md={7} sx={{ px: { xs: 1, md: 0 } }}>
             <Typography variant="h2">Our Mission</Typography>
             <Typography mb={5} mt={1}>
               Our mission at ACCON Engineering is to deliver exceptional road
@@ -65,6 +78,30 @@ const AboutUs2 = () => {
               project managers, and construction workers who work together to
               ensure the success of our projects.
             </Typography>
+            <Grid container spacing={2} sx={{ width: "100%", mt: 2 }}>
+              {[...Array(4)].map((_, index) => (
+                <Grid key={index} item xs={6} sm={3} md={3}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Avatar
+                      sx={{ width: 120, height: 120 }}
+                      src={UserProfileIcon}
+                    />
+                    <Typography fontWeight={800} textAlign={"center"}>
+                      Nilesha Jayanath
+                    </Typography>
+                    <Typography textAlign={"center"}>
+                      Project Manager
+                    </Typography>
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
           </Grid>
         </Grid>
       </Box>
