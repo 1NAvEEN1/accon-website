321d53ddad6b402957792aca7d9147dfe5a5138c
Naveen Jayathilake <tharindunaveen9@gmail.com>
2025-08-29 05:21:46 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const timestamp = new Date().getTime();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,15 +10,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
+        entryFileNames: `assets/[name]-[hash]${timestamp}.js`,
+        chunkFileNames: `assets/[name]-[hash]${timestamp}.js`,
+        assetFileNames: `assets/[name]-[hash]${timestamp}[extname]`,
       },
     },
   },
